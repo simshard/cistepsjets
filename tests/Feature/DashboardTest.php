766481ea4aecc012deee1c52110d44636989d8f0
Simon K <simshard@gmail.com>
2025-08-21 01:00:37 +0100

    trivial change to test workflow

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -8,6 +8,9 @@ test('guests are redirected to the login page', function () {
     $this->get('/dashboard')->assertRedirect('/login');
 });
 
+/**
+ * Trivial Changes 
+ */
 test('authenticated users can visit the dashboard', function () {
     $this->actingAs($user = User::factory()->create());
 
